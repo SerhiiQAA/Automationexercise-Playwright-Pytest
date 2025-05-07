6b9d56b2fd586e828ef137bdb26deb517ad54c74
SerhiiQAA <SerhiiQAA@users.noreply.github.com>
2025-05-07 10:26:03 +0000

    deploy: 585091c43a84cf1d6c395e8f5bc50a44318dfdc9

--- pages/base_page.py
+++
@@ -1,6 +0,0 @@
-class BasePage:
-    def __init__(self, page):
-        self.page = page
-
-    def url_contains(self, partial_url: str) -> bool:
-        return partial_url in self.page.url
--- utils/helper.py
+++
@@ -1,3 +0,0 @@
-# Містить допоміжні функції
-def wait_for_element(page, selector, timeout=5000):
-    page.wait_for_selector(selector, timeout=timeout)
