afcaaa39f769026b5180a6ad94863d0d931461e9
SerhiiQA <nabukve@gmail.com>
2025-05-09 12:46:02 +0300

    run allure

--- test/test_10_verify_subscription_home_page.py
+++ test/test_10_verify_subscription_home_page.py
@@ -38,39 +38,3 @@ def test_verify_subscription_home_page(base_url, browser):
         take_screenshot(page, "Subscription Success Message")
 
     page.close()
-
-
-
-
-# from pages.home_page import HomePage
-# from playwright.sync_api import expect
-# from faker import Faker
-# from utils.take_screenshot import take_screenshot
-
-# def test_verify_subscription_home_page(base_url, browser):
-#     fake = Faker()
-#     page = browser.new_page()
-#     home = HomePage(page, base_url)
-
-#     # 1-2. Navigate to correct url
-#     home.navigate()
-
-#     # 3. Verify that home page is visible successfully
-#     assert home.is_home_page_visible()
-
-#     # 4. Scroll down to footer
-#     home.subscription_title.scroll_into_view_if_needed()
-
-#     # 5. Verify text 'SUBSCRIPTION'
-#     expect(home.subscription_title).to_contain_text("Subscription")
-
-#     # 6. Enter email address in input and click arrow button
-#     test_email = fake.email()
-#     home.email_input_field.fill(test_email)
-#     home.subscribe_button.click()
-
-#     # 7. Verify success message 'You have been successfully subscribed!' is visible
-#     expect(home.subscription_success_message).to_contain_text("You have been successfully subscribed!")
-#     take_screenshot(page, "You have been successfully subscribed!")
-
-#     page.close()
--- test/test_7_verify_test_cases_page.py
+++ test/test_7_verify_test_cases_page.py
@@ -29,32 +29,3 @@ def test_verify_test_cases_page(base_url, browser):
         take_screenshot(page, "Test Cases Page Visible")
 
     page.close()
-
-
-
-
-# from pages.home_page import HomePage
-# from pages.test_cases_page import TCPage
-# from playwright.sync_api import expect
-# from utils.take_screenshot import take_screenshot
-
-# def test_verify_test_cases_page(base_url, browser):
-#     page = browser.new_page()
-#     home = HomePage(page, base_url)
-#     test_cases = TCPage(page)
-
-#     # 1-2. Navigate to correct url
-#     home.navigate()
-
-#     # 3. Verify that home page is visible successfully
-#     assert home.is_home_page_visible()
-
-#     # 4. Click on 'Test Cases' button
-#     home.click_test_cases()
-
-#     # 5. Verify user is navigated to test cases page successfully
-#     assert test_cases.url_contains("/test_cases")
-#     expect(test_cases.title).to_contain_text("Test Cases")
-#     take_screenshot(page, "Test Cases")
-
-#     page.close()
